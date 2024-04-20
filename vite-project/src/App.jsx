cb0716b764ee60264ad7e8e7576c0e8604612f88
Vinicius Rodrigues Varela <rodigues.vv@gmail.com>
2024-04-20 00:01:40 -0300

    Add texts and styling

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-// import profile from "./assets/profile.jpg"
+import profile from "./assets/photo.png";
+import profilebg from "./assets/small.png";
 
 function App() {
   return (
@@ -36,16 +37,26 @@ function App() {
       </header>
       <main>
         <section>
-          <div className="container m-auto px-4 py-10 flex">
-              <div>
-
-              </div>
-              <div>
-                
-              </div>
+          <div className="container m-auto px-4 py-10 flex gap-6  ">
+            <div>
+              <h2 className="font-bold text-3xl">Olá, Eu sou Vinicius,</h2>
+              <h2 className="font-bold text-3xl mt-1 gradiant-text">Frontend Developer.</h2>
+              <p className="mt-4 text-gray-400">
+                Sou um profissional dedicado, com conhecimento sólido em
+                desenvolvimento web e habilidades técnicas bem fundamentadas.
+                Com experiência anterior como QA, possuo uma compreensão
+                abrangente do ciclo de vida do desenvolvimento de software. Meu 
+                objetivo é aplicar minhas habilidades técnicas e conhecimento
+                para contribuir efetivamente como desenvolvedor.
+              </p>
+            </div>
+            <div className="relative">
+              <img src={profile} width={520} />
+              <img src={profilebg} className="absolute top-0 left-1" />
+            </div>
           </div>
         </section>
-      </main>
+      </main> 
     </>
   );
 }
